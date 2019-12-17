13610ffff97f7a6851dfbffeb2744cb50916b3a8
David Markel <davidgmarkel@gmail.com>
2019-12-17 17:50:58 -0500

    add componentDidMount, debug render method in app.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
 
-function App() {
+class App extends Component {
   state = {
-    hello: null
-  }
+      hello: null
+    }
 
   componentDidMount() {
     axios.get('/hello')
-      .then(res => this.setState({hello: res.data}))
-      .catch(err => console.log(err));
-  };
+      .then(res => this.setState({hello: res.data}) )
+      .catch(err => console.log(err) )
+  }
 
-  return (
-    <div>
-      { this.state.hello
-        ? <div>{this.state.hello}
-        : null
-      }
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        { this.state.hello
+          ? <div>{this.state.hello}</div>
+          : null
+        }
+      </div>
+    );
+  }
+
 }
 
 export default App;

16ad69a93f73bd340112fc82ba1b1017868a59b8
David Markel <davidgmarkel@gmail.com>
2019-12-17 17:32:35 -0500

    import axios to App.js, add state with prop hello, add ternary to display hello if !== null

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  state = {
+    hello: null
+  }
+  
   return (
-
+    <div>
+      { this.state.hello
+        ? <div>{this.state.hello}
+        : null
+      }
+    </div>
   );
 }
 
